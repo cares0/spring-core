980f21f186fd9196e20add63cd243a15994e1210
cares0 <cares00000@gmail.com>
2022-04-27 21:53:01 +0900

    객체 지향 설계 원칙을 지키도록 변경

--- src/main/java/spring/core/MemberApp.java
+++ src/main/java/spring/core/MemberApp.java
@@ -8,7 +8,9 @@ import spring.core.member.MemberServiceImpl;
 public class MemberApp {
 
     public static void main(String[] args) {
-        MemberService memberService = new MemberServiceImpl();
+        AppConfig appConfig = new AppConfig();
+        MemberService memberService = appConfig.memberService();
+        // MemberService memberService = new MemberServiceImpl(); AppConfig 사용 이전
 
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
