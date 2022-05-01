2196cc5d856616ba8fee92fc2abf18e326686e1c
cares0 <cares00000@gmail.com>
2022-05-01 15:49:25 +0900

    모든 구현체를 Map, List로 받기

---
+++ src/test/java/spring/core/autowired/AllBeanTest.java
@@ -0,0 +1,47 @@
+package spring.core.autowired;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import spring.core.AutoAppConfig;
+import spring.core.discount.DiscountPolicy;
+import spring.core.member.Grade;
+import spring.core.member.Member;
+
+import java.util.List;
+import java.util.Map;
+
+public class AllBeanTest {
+
+    @Test
+    void findAllBean() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AutoAppConfig.class, DiscountService.class);
+
+        DiscountService discountService = ac.getBean(DiscountService.class);
+        Member member = new Member(1L, "userA", Grade.VIP);
+        int fixDiscountPrice = discountService.discount(member, 10000, "fixDiscountPolicy");
+        Assertions.assertThat(fixDiscountPrice).isEqualTo(1000);
+
+        int rateDiscountPolicy = discountService.discount(member, 20000, "rateDiscountPolicy");
+        Assertions.assertThat(rateDiscountPolicy).isEqualTo(2000);
+    }
+
+    static class DiscountService {
+        private final Map<String, DiscountPolicy> policyMap;
+        private final List<DiscountPolicy> policyList;
+
+        @Autowired
+        public DiscountService(Map<String, DiscountPolicy> policyMap, List<DiscountPolicy> policyList) {
+            this.policyMap = policyMap;
+            this.policyList = policyList;
+            System.out.println("policyMap = " + policyMap);
+            System.out.println("policyList = " + policyList);
+        }
+
+        public int discount(Member member, int price, String discountCode) {
+            DiscountPolicy discountPolicy = policyMap.get(discountCode);
+            return discountPolicy.discount(member, price);
+        }
+    }
+}
