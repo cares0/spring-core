68f278e52e8ef70abb3d5ea6488daa0c843aefb2
cares0 <cares00000@gmail.com>
2022-05-02 15:00:26 +0900

    request 스코프와 프록시

--- src/main/java/spring/core/common/MyLogger.java
+++ src/main/java/spring/core/common/MyLogger.java
@@ -1,6 +1,7 @@
 package spring.core.common;
 
 import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
@@ -8,7 +9,8 @@ import javax.annotation.PreDestroy;
 import java.util.UUID;
 
 @Component
-@Scope(value = "request")
+// 값이 하나이면 그냥 request만 써도 되는데 두 개 이상 들어가면 value로 지정해야함
+@Scope(value = "request", proxyMode = ScopedProxyMode.TARGET_CLASS)
 public class MyLogger {
 
     private String uuid;
--- src/main/java/spring/core/web/LogDemoController.java
+++ src/main/java/spring/core/web/LogDemoController.java
@@ -15,13 +15,14 @@ import javax.servlet.http.HttpServletRequest;
 public class LogDemoController {
 
     private final LogDemoService logDemoService;
-    private final ObjectProvider<MyLogger> myLoggerObjectProvider;
+    private final MyLogger myLogger;
 
     @RequestMapping("log-demo")
     @ResponseBody
     public String logDemo(HttpServletRequest request) {
-        MyLogger myLogger = myLoggerObjectProvider.getObject();
         String requestURL = request.getRequestURL().toString();
+
+        System.out.println("myLogger = " + myLogger.getClass());
         myLogger.setRequestURL(requestURL);
 
         myLogger.log("Controller Test");
